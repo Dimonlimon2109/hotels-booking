5e74705a42c899c9cd9ad8e4e0ba4de495395f9f
Dimonlimon2109 <dimahatcenok@gmail.com>
2025-06-29 20:15:46 +0300

    feat: add WebHostAdapter

---
+++ HotelsBooking.API/Adapters/WebHostAdapter.cs
@@ -0,0 +1,14 @@
+﻿using HotelsBooking.BLL.Interfaces;
+
+namespace HotelsBooking.API.Adapters
+{
+    public class WebHostAdapter : IImagePath
+    {
+        private readonly IWebHostEnvironment _environment;
+        public WebHostAdapter(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+        public string RootPath => _environment.WebRootPath;
+    }
+}
