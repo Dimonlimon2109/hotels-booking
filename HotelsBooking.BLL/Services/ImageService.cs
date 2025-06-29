a9bf948455d102269389245de9a6afc9a10c909d
Dimonlimon2109 <dimahatcenok@gmail.com>
2025-06-29 20:13:07 +0300

    feat: add ImageService with upload and delete methods

--- HotelsBooking.BLL/Services/ImageService.cs
+++ HotelsBooking.BLL/Services/ImageService.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using HotelsBooking.BLL.Interfaces;
 
 namespace HotelsBooking.BLL.Services
 {
-    internal class ImageService
+    public class ImageService
     {
+        private readonly IImagePath _rootPath;
+        private readonly string _imageDirectory = "images";
+        public ImageService(IImagePath rootPath)
+        {
+            _rootPath = rootPath;
+        }
+
+        public async Task<string> UploadImageAsync(IImageFile image, string directory, CancellationToken ct = default)
+        {
+            if (!IsImage(image))
+            {
+                throw new ArgumentException("Переданный файл не является поддерживаемым изображением");
+            }
+            var uploadRoot = Path.Combine(_rootPath.RootPath, directory);
+            Directory.CreateDirectory(uploadRoot);
+
+            var uniqueFileName = $"{Guid.NewGuid()}-{image.FileName}";
+            var filePath = Path.Combine(uploadRoot, uniqueFileName);
+            using (FileStream fs = new FileStream(filePath, FileMode.Create))
+            {
+                await image.CopyToAsync(fs, ct);
+            }
+
+            return Path.Combine(_imageDirectory, directory, uniqueFileName);
+        }
+
+        public Task DeleteImageAsync(string imagePath, CancellationToken ct)
+        {
+            var filePath = Path.Combine (_rootPath.RootPath, imagePath);
+
+            if (File.Exists(filePath))
+                File.Delete(filePath);
+
+            return Task.CompletedTask;
+        }
+        private static bool IsImage(IImageFile file)
+        {
+            var validImageTypes = new[] { "image/jpeg", "image/jpg", "image/png", "image/webp" };
+            return validImageTypes.Contains(file.ContentType);
+        }
     }
 }
