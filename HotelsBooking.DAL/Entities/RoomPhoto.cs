c9f9459617660501a9587c4121779d59ec326d8c
Dimonlimon2109 <dimahatcenok@gmail.com>
2025-07-04 15:50:54 +0300

    feat: add Room and RoomPhoto entities

---
+++ HotelsBooking.DAL/Entities/RoomPhoto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelsBooking.DAL.Entities
+{
+    internal class RoomPhoto
+    {
+    }
+}
